f8c75f871b4094300bba34cc8ea854e4b5399e89
Eamonn de Leastar <edeleastar@wit.ie>
2020-09-29 16:11:16 +0100

    paired back version of sapper starter app

---
+++ src/routes/blog/[slug].svelte
@@ -0,0 +1,21 @@
+<script context="module">
+import {getPost} from './store'
+
+export async function preload({ params }) {
+  return {post : getPost(params.slug)}
+}
+</script>
+
+<script>
+	export let post;
+</script>
+
+<svelte:head>
+	<title>{post.title}</title>
+</svelte:head>
+
+<h1>{post.title}</h1>
+
+<div class="content">
+	{@html post.html}
+</div>
---
+++ src/routes/blog/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">	
+
+import {posts} from './store.js'
+export function preload() {
+  return { posts };  
+}
+</script>
+
+<script>
+	export let posts;
+</script>
+
+<svelte:head>
+	<title>Blog</title>
+</svelte:head>
+
+<h1>Recent posts</h1>
+
+<ul>
+	 {#each posts as post}
+	 	<li><a href="blog/{post.slug}">{post.title}</a></li>
+	 {/each}
+</ul>
